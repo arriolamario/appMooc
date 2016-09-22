f80bec9c44104b58cfbc17ba9117c558895682bd
Mario Arriola <arriola@inworx.corp>
2016-09-22 18:12:39 -0300

    asd

--- prueba.php
+++ prueba.php
@@ -1,27 +1,20 @@
-<?php 
-    echo "empieza<br>";
-    if(isset($_POST["fecha"])){
-        echo "entra<br>";
-        $fecha = $_POST["fecha"];
-        echo "$fecha";
-    }else{
-        echo "seteamos";
-        $fecha = "2016-05-05";
-    }
-?>
-
-
 <html>
-    <head>
-    </head>
-    <body>
-        <form action="" method ="POST">
-            <?php echo "$fecha<br>"?>
-            <input name="fecha" type="date" value="<?php echo date($fecha);?>"      >
-            <scrip>
-                
-            </scrip>
-            <input type="submit">
-        </form>
-    </body>
-</html5>
+<head>
+<title>Traer al Frente</title>
+<script language="javascript">
+var ventana;
+
+function crearVentana(url) {
+    if (!ventana || ventana.closed) {
+        ventana = window.open(url,"hija","status,height=200,width=300");
+    } else {
+        // window is already open, so bring it to the front
+        ventana.focus();
+    }
+}
+</script>
+</head>
+<body>
+ <a href="#" onclick="Javascript:crearVentana('AjaxBasico.html');"> Traer al Frente</a>
+</body>
+</html>
