9f57f1eafb0512ce651da94c0baa9a7e50e2ca97
Arriola Mario <arriola.mario.90@gmail.com>
2016-10-12 14:51:19 -0300

    asd

--- CapaLogica/RegistrarIniciar.php
+++ CapaLogica/RegistrarIniciar.php
@@ -1,5 +1,6 @@
 <?php
 include_once $_SERVER['DOCUMENT_ROOT'] . '/appMooc/Clases/Estudiante.php';
+include_once $_SERVER['DOCUMENT_ROOT'] . '/appMooc/CapaDatos/conexion.php';
 if(isset($_POST["registrar"]))
 {
     $apellido = $_POST["apellido"];
@@ -28,6 +29,35 @@ if(isset($_POST["iniciar"])){
 
     $u = new Usuario($email, $password);
 }
+
+
+    public function login($email, $password){
+        $parametros["email"] = "'$email'";
+        $parametros["password"] = "'$password'";
+
+        $result = select($this->tabla, $parametros);
+        if($result){
+            $idRol;
+            while($fila = mysqli_fetch_assoc($result)){
+                $idRol = $fila["idRol"];
+            }
+
+            switch ($idRol) {
+                case '1': //
+                    # code...
+                    break;
+                case '2':
+                    # code...
+                    break;
+                case '3':
+                    # code...
+                    break;
+                default:
+                    # code...
+                    break;
+            }
+        }
+    }
 
 
 
